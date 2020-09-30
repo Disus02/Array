f7a99bc0b8d950f07b1745b55986701275094843
Danil Stozhkov <lekseih2000@mail.ru>
2020-09-30 11:34:41 +0300

    Create file

--- src/ru/sapteh/Program.java
+++ src/ru/sapteh/Program.java
@@ -1,12 +1,17 @@
 package ru.sapteh;
 
 import javax.imageio.IIOException;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Program {
     public static void main(String[] args) throws IOException {
+        File dir = new File("C:/Java_42_t/1/Person");
+        File file = new File("C:/Java_42_t/1/Person/Per.txt");
+        dir.mkdir();
+        file.createNewFile();
+
+        FileWriter fos = new FileWriter(file);
+
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите имя: ");
         String name = buffer.readLine();
@@ -14,8 +19,12 @@ public class Program {
         int age = Integer.parseInt(buffer.readLine());
         System.out.println("Введите дату: ");
         int date = Integer.parseInt(buffer.readLine());
+
         Person person = new Person(name, age, date);
         System.out.println(personIsEmpty(person));
+
+        fos.write(personIsEmpty(person));
+        fos.close();
 
     }
 
